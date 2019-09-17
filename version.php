8b3015bbfc04da53222a7a6ddd92c952ab67d354
callmngr <gbateson@users.noreply.github.com>
2019-09-17 14:27:10 +0000

    tool_importusers new functionality to enrol users into required courses and groups

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_importusers';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2019091505;
-$plugin->release   = '2019-09-15 (05)';
+$plugin->version   = 2019091707;
+$plugin->release   = '2019-09-17 (07)';
