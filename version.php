aa377921cbcca501876cc210d9640d80199640c1
Gordon Bateson <gordonbateson@gmail.com>
2020-09-23 23:25:42 +0900

    tool_importusers improve parsing of functions in XML file, and allow user to choose between adding login details to a Book resource or a Page resource.

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_importusers';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2020101719;
-$plugin->release   = '2020-10-17 (19)';
+$plugin->version   = 2020102320;
+$plugin->release   = '2020-10-23 (20)';
